6db8eb0e7aee64d7cab76ae79c294aaa302ea559
deepak261 <57754152+deepak261@users.noreply.github.com>
2019-11-18 03:54:15 -0500

    hello_world.py

---
+++ hello word.py
@@ -0,0 +1,17 @@
+'''
+S01Q01
+
+Re-write the earlier question of S01Q01 using functions as mentioned below :
+
+  - Modify the first “Hello World” program to prompt for user’s name 
+           and then wish the user by saying Hello followed by his name
+
+           Example :
+           If user's name is Sam, then expected output is :
+           Hello Sam !!!
+'''
+
+
+
+name = input("enter the name:")
+print("hello",name)
