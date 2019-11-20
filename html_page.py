2f9810daebdc128e12140673da96e443b744d3a8
deepak261 <57754152+deepak261@users.noreply.github.com>
2019-11-19 23:57:59 -0500

    html_page.py

---
+++ html_page.py
@@ -0,0 +1,29 @@
+'''
+S09Q01
+
+Write a program that generates a HTML webpage. 
+Prompt the user for webpage title and webpage body contents. 
+The webpage body should contain 
+ - one main header, 
+ - one sub header, and 
+ - at least 2 paragraphs.
+'''
+
+print('                       webpage.html\n                       ------------')
+print('<html>')
+print(' <head>')
+print('    <title>')
+print('          My webpage title')
+print('    </title>')
+print(' </head>')
+print('\n <body>')
+print('    <h1>Webpage Banner</h1>')
+print('\n    <p>This is a sample story\n       for my first webpage')
+print('    </p>')
+print('\n    <p>This is a how my sample\n       story ends')
+print('    </p>')
+print(' </body>')
+print('</html>')
+
+      
+
